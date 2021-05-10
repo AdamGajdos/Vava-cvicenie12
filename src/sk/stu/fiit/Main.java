3567037cea8097b1aec78f2bae73172050965588
adamg <adamg@LAPTOP-QJHOJUOI>
2021-05-10 19:27:19 +0200

    pridana kontrola regexu

---
+++ src/sk/stu/fiit/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
+/**
+ *
+ * @author adamg
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        gui.Obrazovka obr = new gui.Obrazovka();
+        obr.setVisible(true);
+      
+    
+    }
+    
+}
